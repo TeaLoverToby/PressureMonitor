db247c1e1e8124f38ec8f8c1ea1d8c648b422abf
TeaLoverToby <68970405+TeaLoverToby@users.noreply.github.com>
2025-11-15 23:12:21 +0000

    Add User model with properties for Id, Username, Email, and Password

---
+++ PressureMonitor/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PressureMonitor.Models;
+
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    // The string length is specified to prevent long names + memory issues
+    [StringLength(50)]
+    public string? Username { get; set; }
+    
+    [StringLength(100)]
+    public string? Email { get; set; }
+    
+    [StringLength(255)]
+    public string? Password { get; set; }
+    
+}
