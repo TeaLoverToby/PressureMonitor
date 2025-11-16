4ccca9639b31106f07fdd50f8d7d387e8fcb3b83
TeaLoverToby <68970405+TeaLoverToby@users.noreply.github.com>
2025-11-16 17:06:36 +0000

    Add PressureMap/PressureFrame relationships to ApplicationDbContext

--- PressureMonitor/Models/ApplicationDbContext.cs
+++ PressureMonitor/Models/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ public class ApplicationDbContext : DbContext
     public DbSet<Clinician> Clinicians { get; set; }
     public DbSet<Patient> Patients { get; set; }
     public DbSet<Admin> Admins { get; set; }
+    public DbSet<PressureMap> PressureMaps { get; set; }
+    public DbSet<PressureFrame> PressureFrames { get; set; }
 
     // This method allows us to configure the relationships between the entities (e.g foreign keys, cardinality and that sort of thing)
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -34,6 +36,20 @@ public class ApplicationDbContext : DbContext
             .HasOne(u => u.Admin)
             .WithOne(a => a.User)
             .HasForeignKey<Admin>(a => a.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        // Patient -> PressureMap (one-to-many)
+        modelBuilder.Entity<Patient>()
+            .HasMany(p => p.PressureMaps)
+            .WithOne(pm => pm.Patient)
+            .HasForeignKey(pm => pm.PatientId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        // PressureMap -> PressureFrame (one-to-many)
+        modelBuilder.Entity<PressureMap>()
+            .HasMany(pm => pm.Frames)
+            .WithOne(pf => pf.PressureMap)
+            .HasForeignKey(pf => pf.PressureMapId)
             .OnDelete(DeleteBehavior.Cascade);
         
         base.OnModelCreating(modelBuilder); 
