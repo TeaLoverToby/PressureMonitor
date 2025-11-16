ead64379ed546b134e0b80d6dbe0a62f6ba04313
TeaLoverToby <68970405+TeaLoverToby@users.noreply.github.com>
2025-11-16 05:46:26 +0000

    Add Admin model - need to finish this

---
+++ PressureMonitor/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PressureMonitor.Models;
+
+public class Admin
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; } = null!;
+
+    // Add stuff here in the future
+}
