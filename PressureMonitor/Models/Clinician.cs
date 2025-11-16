804ce066a6c1dc5b1b240aaff94b61a9e53e2f9a
TeaLoverToby <68970405+TeaLoverToby@users.noreply.github.com>
2025-11-16 05:49:43 +0000

    Added Clinician Model

---
+++ PressureMonitor/Models/Clinician.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PressureMonitor.Models;
+
+public class Clinician
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public int UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; } = null!;
+
+    [StringLength(100)]
+    public string? LicenseNumber { get; set; }
+    
+}
+
