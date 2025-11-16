d23d7600d91c7860eb3f6e1590c8d6fec39822de
TeaLoverToby <68970405+TeaLoverToby@users.noreply.github.com>
2025-11-16 05:47:58 +0000

    Added Patient model

---
+++ PressureMonitor/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PressureMonitor.Models;
+
+public class Patient
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public int UserId { get; set; }
+    
+    [ForeignKey("UserId")]
+    public User User { get; set; } = null!;
+    
+    public DateTime? DateOfBirth { get; set; }
+    
+}
+
