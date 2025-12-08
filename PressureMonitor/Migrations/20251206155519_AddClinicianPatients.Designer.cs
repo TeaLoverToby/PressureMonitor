5a62948c4498420f15b51a938048a1ed55ea410a
TeaLoverToby <68970405+TeaLoverToby@users.noreply.github.com>
2025-12-08 19:27:45 +0000

    Added missing migrations

--- PressureMonitor/Migrations/20251206155519_AddClinicianPatients.Designer.cs
+++ PressureMonitor/Migrations/20251206155519_AddClinicianPatients.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PressureMonitor.Models;
 
@@ -10,9 +11,11 @@ using PressureMonitor.Models;
 namespace PressureMonitor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251206155519_AddClinicianPatients")]
+    partial class AddClinicianPatients
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
---
+++ PressureMonitor/Migrations/20251206155519_AddClinicianPatients.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PressureMonitor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClinicianPatients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClinicianId",
+                table: "Patients",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_ClinicianId",
+                table: "Patients",
+                column: "ClinicianId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_Clinicians_ClinicianId",
+                table: "Patients",
+                column: "ClinicianId",
+                principalTable: "Clinicians",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_Clinicians_ClinicianId",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_ClinicianId",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "ClinicianId",
+                table: "Patients");
+        }
+    }
+}
