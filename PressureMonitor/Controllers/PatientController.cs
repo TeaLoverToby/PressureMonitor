ce2685125d24ac348b8b24bec9e2df6f3c10b70e
Salix03 <fv161@student.ac.uk>
2025-12-09 11:06:44 +0000

    Added comment functionality

--- PressureMonitor/Controllers/PatientController.cs
+++ PressureMonitor/Controllers/PatientController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PressureMonitor.Models;
+using Microsoft.Data.Sqlite;
 
 namespace PressureMonitor.Controllers;
 
@@ -31,6 +32,115 @@ public class PatientController(ILogger<PatientController> logger, ApplicationDbC
         return View(patient);
     }
 
+    [HttpPost]
+    [Route("save-comment")]
+    public IActionResult SaveComment([FromBody] Comment comment)
+    {
+        var userIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (string.IsNullOrWhiteSpace(userIdStr) || !int.TryParse(userIdStr, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        try
+        {
+            using (var connection = new SqliteConnection("Data Source=PressureMonitor.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                    @"
+                        INSERT INTO Comments 
+                        (UserId, MapId, ParentId, Text, CreatedAt)
+                        VALUES 
+                        ($userId, $mapId, $parentId, $text, $createdAt)
+                    ";
+                command.Parameters.AddWithValue("$userId", userId);
+                command.Parameters.AddWithValue("$mapId", comment.MapId);
+
+                if (comment.ParentId == null)
+                    command.Parameters.AddWithValue("$parentId", DBNull.Value);
+                else
+                    command.Parameters.AddWithValue("$parentId", comment.ParentId);
+
+                command.Parameters.AddWithValue("$text", comment.Text);
+                command.Parameters.AddWithValue("$createdAt", DateTime.UtcNow);
+
+                command.ExecuteNonQuery();
+            }
+
+            return Ok("Insert successful");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest("DB ERROR: " + ex.Message);
+        }
+    } 
+    
+    [HttpGet]
+
+    [Route("get-recent-comments")]
+    public IActionResult GetRecentComments()
+    {
+        // list to hold our comments
+        var comments = new List<Comment>();
+
+        try
+        {
+            // connect to database
+            using (var connection = new SqliteConnection("Data Source=PressureMonitor.db"))
+            {
+                connection.Open();
+
+                // create an SQL query to get the 10 most recent comments
+                var command = connection.CreateCommand();
+
+                command.CommandText =
+                    @"
+                SELECT Id, UserId, MapId, ParentId, Text, CreatedAt
+                FROM Comments
+                ORDER BY CreatedAt DESC
+                LIMIT 10
+            ";
+
+                // execute the query and read the results
+                using (var reader = command.ExecuteReader())
+                {
+                    // Loop through every row returned by the database
+                    while (reader.Read())
+                    {
+                        // convert database row into c# object
+                        comments.Add(new Comment
+                        {
+                            Id = reader.GetInt32(0),
+
+                            UserId = reader.GetInt32(1),
+
+                            MapId = reader.GetInt32(2),
+
+                            // set parentId to null if null in database
+                            ParentId = reader.IsDBNull(3) ? null : reader.GetInt32(3),
+
+                            Text = reader.GetString(4),
+
+                            //convert createdAt string to dateTime
+                            CreatedAt = DateTime.Parse(reader.GetString(5))
+                        });
+                    }
+                }
+            }
+
+
+            return Ok(comments);
+        }
+        //error handling
+        catch (Exception ex)
+        {
+            return BadRequest("DB ERROR: " + ex.Message);
+        }
+    }
+    
     [HttpGet]
     public async Task<IActionResult> Upload()
     {
---
+++ PressureMonitor/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace PressureMonitor.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public int UserId { get; set; }
+    public int MapId { get; set; }
+    public int? ParentId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
